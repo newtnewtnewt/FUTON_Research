24411d41523a8a1070905a6cf0449a6b1e85e9ec
Noah Dunn <42472588+newtnewtnewt@users.noreply.github.com>
2020-08-30 14:55:11 -0400

    Multithreading is working

---
+++ q_learning_functions.py
@@ -0,0 +1,73 @@
+###
+#  FUTON Model MDP + Q-Learning Creation Script
+#  A Research Project conducted by Noah Dunn 
+###
+
+# Import the standard tools for working with Pandas dataframe
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import shelve
+# Import the MDP toolbox that contains a method for conducting Q-Learning
+# Tool can be found here: https://github.com/sawcordwell/pymdptoolbox
+# Documentation for the tool can be found here 
+import mdptoolbox
+
+###
+# A function to actually perform Q learning
+# REPLACE WITH ACTUAL INFORMATION
+### 
+def offpolicy_Q_learning(ql_train_set, gamma, alpha, numtraces):
+    result_matrix = np.zeros((25, 750))
+    return result_matrix
+
+###
+# parallel_bootql_Creation is a function intended to be used
+# with the multithraeding package availabe in Python
+# It takes arguments passed in from it's parent function: offpolicy_eval_tdlearning
+# unique_training_set_ids: All the patient ids for the training set
+# proportion: The proportion of the data to be used for training
+# gamma:  A multiplier to be used in the Q-learning
+# qlearning_train_dataset_final: The full dataset of value
+# physician_policy: A 25 x 750 matrix of the actions a phyiscian chosen given a state (physician_policy[A][S])
+# distribution_values: A 750 long array that stores frequency of state appears
+###
+def parallel_bootql_creation(unique_training_set_ids, proportion, gamma, 
+                             qlearning_train_dataset_final, physician_policy, distribution_values):
+    # Grab a random sample of the ids to use for the Q-LEARNING step
+    train_len = len(unique_training_set_ids)
+    # We are going to randomly mark around (proportion) number of IDs
+    # and mark them as 1's to filter
+    id_flags = [np.floor(np.random.rand() + proportion) == 1 for i in range(0, train_len)]
+    chosen_ids = unique_training_set_ids[id_flags]
+    # Choose the rows with training_ids begin at the 1
+    ql_train_set_proto = qlearning_train_dataset_final[qlearning_train_dataset_final['training_set_id'].isin(chosen_ids)]
+    ql_train_set = pd.concat((ql_train_set_proto['training_bloc'],
+                             ql_train_set_proto['closest_cluster_index'], 
+                             ql_train_set_proto['chosen_action_index'], 
+                             ql_train_set_proto['reward_value']),
+                             axis=1,
+                             keys=['training_bloc',"closest_cluster_index","chosen_action_index","reward_value"]
+                            )
+    
+            
+    # Use the train set to achieve optimal Q-Equation
+    offpolicy_Q_result = offpolicy_Q_learning(ql_train_set, gamma, 0.1, 30000)
+    
+    # Value the phyisican's decision based on Q-Learning probabilistic 
+    # distribution and the actual chosen policy at each step
+    value_matrix = np.zeros((25, 750))
+    for i in range(0, 25):
+        for j in range(0, 750):
+            value_matrix[i][j] = physician_policy[i][j] * offpolicy_Q_result[i][j]
+    
+    # The reward of a given state is the sum of all possible actions 
+    # That can be taken from that state
+    value_sums = np.zeros(750)
+    for i in range(0, 25):
+        for j in range(0, 750):
+            value_sums[i] = value_sums[i] + value_matrix[i, j]
+    
+    # Return the rewards for each state, weighted by their probability of
+    # occuring (gathered from frequency in the actual data)
+    return np.nansum(np.multiply(value_sums, distribution_values))/sum(distribution_values)
